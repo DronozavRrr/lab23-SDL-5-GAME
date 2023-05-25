967a32bd365436f95bebe813e84f49ebe17112d0
Андрей Гаврилов <79051@DESKTOP-GURLRMB>
2023-05-25 16:33:50 +0300

    last up

--- lab23(SDL-5) GAME/Source.cpp
+++ lab23(SDL-5) GAME/Source.cpp
@@ -1,4 +1,4 @@
-#define BALL_COUNT 5
+#define BALL_COUNT 10
 #pragma comment(lib, "SDL2_mixer.lib")
 #include "SDL.h"
 #include "stdlib.h"
@@ -10,12 +10,14 @@
 #include <iostream>
 #include<malloc.h>
 #include <locale.h>
-//anton loh
+#include <locale.h>
+
 
 struct descriptionBall
 {
 	SDL_Texture* Ball;
 	SDL_Texture* TextOnBall;
+	SDL_Rect size;
 };
 SDL_Texture* get_text_texture(SDL_Renderer*& renderer, char* text, TTF_Font* font)
 {
@@ -52,9 +54,9 @@ void loadmusic()
 void loadmusic2()
 {
 	Mix_Music* fon = Mix_LoadMUS("salam-meloboom.wav");
-	Mix_PlayMusic(fon, 1);
-	SDL_Delay(2500);
-	loadmusic();
+	Mix_PlayMusic(fon, 0);
+	
+	
 }
 
 
@@ -141,6 +143,8 @@ bool is_star_hit(SDL_Rect True_Star, int x, int y)
 
 
 
+
+
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "RUS");
@@ -164,6 +168,7 @@ int main(int argc, char** argv)
 	{
 	case 1:
 	{
+		
 		SDL_Surface* ballImage = IMG_Load("1.bmp");
 		SDL_SetColorKey(ballImage, SDL_TRUE, SDL_MapRGB(ballImage->format, 255, 255, 255));
 		init_balls(balls, BALL_COUNT);
@@ -190,6 +195,7 @@ int main(int argc, char** argv)
 			randnum[i] = rand() % 10;
 		}
 		loadmusic();
+		int gcount = 0;
 		while (!quit)
 		{
 
@@ -203,12 +209,13 @@ int main(int argc, char** argv)
 					{
 						if (is_ball_hit(balls[i], event.button.x, event.button.y))
 						{
-							loadmusic2();
+							 Mix_Chunk* fireMusic = Mix_LoadWAV("salam-meloboom.wav");
+							 Mix_PlayChannel(-1, fireMusic, 0);
 							balls[i].w = balls[i].h = 0;
 							k += randnum[i];
 							_itoa_s(k, text, 10);
 							textTexture = get_text_texture(renderer, text, my_font);
-
+							
 						}
 					}
 				}
@@ -217,31 +224,39 @@ int main(int argc, char** argv)
 			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 			SDL_RenderClear(renderer);
 
-
+			gcount = 0;
 			for (int i = 0; i < BALL_COUNT; ++i)
 			{
-
+				
 				_itoa_s(randnum[i], text1, 10);
 				SDL_Rect textRect;
-
+			
 				test[i].Ball = SDL_CreateTextureFromSurface(renderer, ballImage);
 				test[i].TextOnBall = get_text_texture_balls(renderer, text1, my_font, &textRect);
-				//draw_balls(renderer, balls, BALL_COUNT, test[i].TextOnBall);
 
-				if (balls[i].w == 0) continue;
+				if (balls[i].w == 0)
+				{
+					gcount++;
+					if (gcount == BALL_COUNT) return 0;
+					continue;
+				};
 				SDL_Rect ballrect = { balls[i].x,balls[i].y,balls[i].w,balls[i].h };
 				SDL_RenderCopy(renderer, test[i].Ball, NULL, &ballrect);
 
 				textRect.x = (balls[i].x + int(w / 100.f));
 				textRect.y = balls[i].y + balls[i].h / 4;
 				SDL_RenderCopy(renderer, test[i].TextOnBall, NULL, &textRect);
+				SDL_DestroyTexture(test[i].Ball);
+				SDL_DestroyTexture(test[i].TextOnBall);
+
 			}
 			/* Игровая логика */
 			draw_text(renderer, textTexture);
 			/* Отрисовка сцены */
 			SDL_RenderPresent(renderer);
-			SDL_Delay(1000);
+			SDL_Delay(500);
 		}
+
 		SDL_DestroyTexture(textTexture);
 		TTF_CloseFont(my_font);
 		TTF_Quit();
@@ -253,56 +268,130 @@ int main(int argc, char** argv)
 	}
 	case 2:
 	{
-
-		SDL_Texture* Ball[BALL_COUNT];
+		
+		Mix_Init(0);
+		Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 1024);
+		descriptionBall test[BALL_COUNT];
 		SDL_Surface* ballImage = IMG_Load("1.bmp");
 		SDL_SetColorKey(ballImage, SDL_TRUE, SDL_MapRGB(ballImage->format, 255, 255, 255));
-		init_balls(balls, BALL_COUNT);
-		SDL_Rect ballrect = { 0,0,0,0 };
-		double x = 0; double y = 0;
-		const double AMPLITUDE = 200.0;
-		const double PERIOD = 500.0;
-		const double SPEED = 0.005;
-		double time = 0.0;
+		int randnum[BALL_COUNT];
 		for (int i = 0; i < BALL_COUNT; ++i)
 		{
-
-			Ball[i] = SDL_CreateTextureFromSurface(renderer, ballImage);
+			randnum[i] = rand() % 10;
 		}
-		while (true)
+		SDL_Rect ballrect[BALL_COUNT];
+		SDL_Rect textRect;
+		double x = 0; double y = 0;
+		const double AMPLITUDE = 100.0;
+		const double PERIOD = 600.0;
+		const double STEP = 5.0;
+		double time = 0.5;
+		char text1[10];
+
+		bool quit = false;
+
+
+		int k = 0;
+		char text[10];
+		
+
+		_itoa_s(k, text, 10);
+		textTexture = get_text_texture(renderer, text, my_font);
+		SDL_Event event;
+		
+		int randval[BALL_COUNT];
+		
+		char name[] = "1.wav";
+		char textballs[10];
+		double hh = 0;
+		loadmusic();
+		for (int i = 0; i < BALL_COUNT; ++i)
 		{
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-			SDL_RenderClear(renderer);
-			SDL_Event event;
-			while (SDL_PollEvent(&event))
-			{
+			_itoa_s(randnum[i], text1, 10);
+			test[i].Ball = SDL_CreateTextureFromSurface(renderer, ballImage);
+			test[i].TextOnBall = get_text_texture_balls(renderer, text1, my_font, &textRect);
+			x = (i) * ((w / (double)BALL_COUNT));
+			y = AMPLITUDE * sin(2 * M_PI * (x / PERIOD + time));
+			test[i].size.x = x;
+			ballrect[i] = { int(x) ,int(y + 300),50,50 };
+			textRect.x = (ballrect[i].x + int(w / 100.f));
+			textRect.y = ballrect[i].y + ballrect[i].h / 4;
+
+			
+		}
+
+		while (!quit)
+		{
+			
+			while (SDL_PollEvent(&event)) {
 				if (event.type == SDL_QUIT)
+					quit = true;
+
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
 				{
-					return 1;
+					for (int i = 0; i < BALL_COUNT; i++)
+					{
+						if (is_ball_hit(ballrect[i], event.button.x, event.button.y))
+						{
+							 Mix_Chunk* fireMusic = Mix_LoadWAV("salam-meloboom.wav");
+							 Mix_PlayChannel(-1, fireMusic, 0);loadmusic2();
+							 ballrect[i].w = ballrect[i].h = 0;
+							 k += randnum[i];
+							_itoa_s(k, text, 10);
+							textTexture = get_text_texture(renderer, text, my_font);
+
+						}
+					}
 				}
+
 			}
+			hh += 0.5;
 			for (int i = 0; i < BALL_COUNT; ++i)
 			{
-
-				x = i * (w / (double)BALL_COUNT);
+				
+				if (ballrect[i].w == 0) continue;
+				test[i].size.x += STEP;
+				
 				y = AMPLITUDE * sin(2 * M_PI * (x / PERIOD + time));
-				ballrect.x = x;
-				ballrect.y = y;
-				ballrect = { int(x),int(y),rand() % 100 + 50,rand() % 100 + 50 };
-				SDL_RenderCopy(renderer, Ball[i], NULL, &ballrect);
+				if (ballrect[i].x >= 820)
+				{
+					ballrect[i].x = 0;
+					
+					test[i].size.x = 0;
+					y = 300;
+			
+				}
+				
+			x = (i) * ((w / (double)BALL_COUNT));
+				
+				
+				ballrect[i] = {test[i].size.x ,int(y + 300),rand() %(70- 40) + 40,50};
+				
+			
 
+				SDL_RenderCopy(renderer, test[i].Ball, NULL, &ballrect[i]); // рисуем шарик в новом мест
+				
+			
+				textRect.x = (ballrect[i].x + int(w / 100.f));
+				textRect.y = ballrect[i].y + ballrect[i].h / 4;
+				
+				SDL_RenderCopy(renderer, test[i].TextOnBall, NULL, &textRect);
+				
 
 			}
+			
 
-
+			draw_text(renderer, textTexture);
 			SDL_RenderPresent(renderer);
-			time += SPEED;
-			SDL_Delay(50);
+			SDL_Delay(100);
+			time += STEP;
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+			SDL_RenderClear(renderer);
 		}
 
 
 
-	}
+	}break;
 	case 3:
 	{
 		Mix_Init(0);
@@ -320,7 +409,7 @@ int main(int argc, char** argv)
 		SDL_Rect* stars = (SDL_Rect*)malloc(sizeof(SDL_Rect) * 5);
 		int a[5], b[5];
 
-		char text[100];
+		char text[100] = {0};
 		int count = 0;
 		_itoa_s(count, text, 10);
 		textTexture = get_text_texture(renderer, text, my_font);
@@ -400,9 +489,25 @@ int main(int argc, char** argv)
 									break;
 								}
 							}
-							if (try1 >= 3000)
+							if (count >= 15)
 							{
-								std::cout << "Победа:" << n;
+								setlocale(LC_ALL, "RUS");
+								SDL_DestroyWindow(window);
+								SDL_Window* window = SDL_CreateWindow("End",
+									SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300,
+									SDL_WINDOW_SHOWN);
+								SDL_Renderer* renderer2 = SDL_CreateRenderer(window, -1, 0);
+
+
+								char text[100] = {0};
+								_itoa_s(count, text, 10);
+								SDL_DestroyTexture(textTexture);
+
+
+								SDL_Texture* end = get_text_texture(renderer2, text, my_font);
+								draw_text(renderer2, end);
+								SDL_RenderPresent(renderer2);
+								SDL_Delay(5000);
 								return 0;
 							}
 
@@ -418,21 +523,28 @@ int main(int argc, char** argv)
 					}
 					else
 					{
-						if(k > 1)
+						if(k>1)
 						{
-							SDL_DestroyWindow(window);
-							SDL_Window* window2 = SDL_CreateWindow("End",
-								SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300,
-								SDL_WINDOW_SHOWN);
-							SDL_Renderer* render = SDL_CreateRenderer(window2,-1,0);
-							_itoa_s(count, text, 10);
-							textTexture = get_text_texture(render, text, my_font);
-							draw_text2(render, textTexture);
-							SDL_RenderPresent(render);
-							SDL_Delay(5000);
-							return 0;
+							
+								SDL_DestroyWindow(window);
+								SDL_Window* window = SDL_CreateWindow("End",
+									SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300,
+									SDL_WINDOW_SHOWN);
+								SDL_Renderer* renderer1 = SDL_CreateRenderer(window, -1, 0);
+								
+								
+								char text[100] = {0};
+								SDL_DestroyTexture(textTexture);
+								
+								_itoa_s(count, text, 10);
+								SDL_Texture *end = get_text_texture(renderer1, text, my_font);
+								draw_text(renderer1, end);
+								SDL_RenderPresent(renderer1);
+								SDL_Delay(5000);
+								return 0;
 
 						}
+				
 					}
 					
 				}
